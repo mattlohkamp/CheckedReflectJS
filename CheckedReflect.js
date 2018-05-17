f50bef33a7d1e6812f9b19c73b2ff25d640c8af0
mattlohkamp <work@mattlohkamp.com>
2018-05-16 17:36:07 -0700

    devolved the function for testing

--- CheckedReflect.js
+++ CheckedReflect.js
@@ -1,5 +1,29 @@
+/*
 function CheckedReflect(nodeList,checkedClass='checked'){
-	nodeList.forEach(input=>input.addEventListener('change',e=>e.target.form.labels.forEach(input=>input.classList[(input.checked) ? 'remove' : 'add'](checkedClass))))
+	nodeList.forEach(input=>input.addEventListener('change',e=>e.target.labels.forEach(label=>label.classList[(input.checked) ? 'remove' : 'add'](checkedClass))))
 }
+*/
 
-CheckedReflect(document.querySelectorAll('input[type=radio],input[type=checkbox]'),'selected')
+function CheckedReflect(nodeList,checkedClass){
+	if(checkedClass == undefined){
+		checkedClass = 'checked'
+	}
+	for(var i = 0; i < nodeList.length; i++){
+		var node = nodeList[i]
+		node.addEventListener('change',function(e){
+			for(var j = 0; j < e.target.form.length; j++){
+				var input = e.target.form[j]
+				if(input.name == e.target.name){
+					for(var k = 0; k < input.labels.length; k++){
+						var label = input.labels[k]
+						if(input.checked == true){
+							label.classList.add(checkedClass)
+						}else{
+							label.classList.remove(checkedClass)
+						}
+					}
+				}
+			}
+		})
+	}
+}
